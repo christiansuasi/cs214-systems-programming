45f47c8061666f0731bbd35a44242a10edf7f3ad
c <cis27@scarletmail.rutgers.edu>
2016-02-06 04:05:44 -0500

    update

--- pa1 tokenizer/tokenizer.c
+++ pa1 tokenizer/tokenizer.c
@@ -1,13 +1,14 @@
-/*
+/*Christian Suasi & Alec Fray
  * tokenizer.c
  */
 #include <stdio.h>
-
+#include <stdlib.h>
 /*
  * Tokenizer type.  You need to fill in the type as part of your implementation.
  */
 
 struct TokenizerT_ {
+	char* input;
 };
 
 typedef struct TokenizerT_ TokenizerT;
@@ -27,8 +28,9 @@ typedef struct TokenizerT_ TokenizerT;
  */
 
 TokenizerT *TKCreate( char * ts ) {
-
-  return NULL;
+	TokenizerT* token = malloc(sizeof(ts));
+	token->input = ts;
+	return token;
 }
 
 /*
@@ -53,8 +55,93 @@ void TKDestroy( TokenizerT * tk ) {
  * You need to fill in this function as part of your implementation.
  */
 
-char *TKGetNextToken( TokenizerT * tk ) {
+char *TKGetNextToken( TokenizerT * tk ){
 
+	char *draft = tk->input; 
+	int i = 0;
+	int j = 0;
+	char *word;
+	int *dec = 0;
+	int *oct = 0;
+	int *hex = 0;
+	
+	//loop through each char in raw input string
+	while(draft[i]!='\0'){
+
+		//getting a word
+		if( isalpha(draft[i])>0 && draft[i+1]!='\0' ){
+			word[j] = draft[i];
+			j++;
+		}else{
+			printf("word \"%s\"\n", word);
+			return word;
+		}
+  	
+		//binary operator switch table	
+		switch(draft[i]){
+			case '(' :
+				printf("left paranthesis \"(\"\n");
+				break;
+			case ')' :
+				printf("right paranthesis \")\"\n");
+				break;
+			case '[' :
+				printf("left bracket \"[\"\n");
+				break;
+			case ']' :
+				printf("right braket \"]\"\n");
+				break;
+			case '*' :
+				printf("indirect or multiply \"*\"\n");
+				break;
+			case '&' :
+				printf("address or bitwise and \"&\"\n");
+				break;
+			case '-' :
+				printf("minus or subtract \"-\"\n");
+				break;
+			case '!' :
+				printf("negate \"!\"\n");
+				break;
+			case '~' :
+				printf("1's compliment \"~\"\n");
+				break;
+			case '+' :
+				printf("add \"+\"\n");
+				break;
+			case '^' :
+				printf("bitwise exclusive or \"^\"\n");
+				break;
+			case '|' :
+				printf("bitwise or \"|\"\n");
+				break;
+			case '/' :
+				printf("divide \"/\"\n");
+				break;
+			case '<' :
+				printf("less than \"<\"\n");
+				break;
+			case '>' :
+				printf("greater than \">\"\n");
+				break;
+			case '%' :
+				printf("modulus \"%\"\n");
+				break;
+			case '\\' :
+				printf("escape character \"\\\"\n");
+				break;
+		
+			default :
+				break;
+			
+		}
+
+
+
+		i++;
+	}
+	
+		
   return NULL;
 }
 
@@ -66,6 +153,30 @@ char *TKGetNextToken( TokenizerT * tk ) {
  */
 
 int main(int argc, char **argv) {
+	
+	//argc stores number of command line args
+	//argv stores the position of actual arg
+	
+	if( argc==2 ){
+		printf("argument is %s\n", argv[1]);
+	}else{
+		printf("missing string input argument\n");
+		exit(1);
+	}
 
-  return 0;
+	
+	//take input from command line
+	char *inputString;	
+	inputString = argv[1];;	
+	//tokenize string
+	
+	TokenizerT *tok = TKCreate(inputString);
+	TKGetNextToken( tok );
+	//printf("print out %s\n", TKGetNextToken(tok));
+
+	//print out each token
+	//loop through tkgetnextoken
+	//finish
+
+	return 0;
 }
